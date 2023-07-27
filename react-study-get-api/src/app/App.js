7ab9e151b0199562ca778c662aea4c14c4ef2a40
Alberto Ventura <albertoventura.dev@gmail.com>
2023-07-27 09:31:30 -0300

    base folders

---
+++ react-study-get-api/src/app/App.js
@@ -0,0 +1,12 @@
+import GetAdvice from '../features/get-advices/GetAdvice';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GetAdvice/>
+    </div>
+  );
+}
+
+export default App;
